b4477bf145eb6bb2cf836e3849d466e7d6833fca
giuli <giulio.pastore2410@gmail.com>
2023-06-09 17:18:17 +0200

    change contratto in contract

--- src/main/java/com/example/TeamProject/controller/SecretaryController.java
+++ src/main/java/com/example/TeamProject/controller/SecretaryController.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping(value = "/api/secretaries/")
+@RequestMapping(value = "/api/secretary/")
 public class SecretaryController {
     private SecretaryService secretaryService;
 
